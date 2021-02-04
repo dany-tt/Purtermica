0e5edbd173918c613ecf8852593723c5a0f0c35c
dany-tt <dantrinchero@gmail.com>
2021-02-04 01:24:07 -0300

    contacto, mapa, textos, wap

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,3 @@
-/* Declaramos tres objetos, como constantes, que cargarán en memoria, 
-respectivamente, en núcleo de gulp y los dos plugins que estamos usando aquí. */
 const gulp = require('gulp'), 
 	sass = require('gulp-sass'), 
 	autoprefixer = require('gulp-autoprefixer');
@@ -7,17 +5,17 @@ const gulp = require('gulp'),
 /* Creamos una tarea de gulp que compila el scss a css, 
 pone los autpoprefijos dónde sean necesarios, y vuelca 
 el resultado en el archivo de destino (el css). */
-gulp.task ('sass-compile', ()=>
-	gulp.src('./scss/*.scss').
-		pipe(sass()).
-		pipe(autoprefixer()).
-		pipe(gulp.dest('./css'))
-);
+gulp.task('sass-compile', function () {
+    return gulp.src('./scss/*.scss')
+        .pipe(sass())
+        .pipe(autoprefixer())
+        .pipe(gulp.dest('./css/'));
+});
  
 /* Establecemos un "vigilante" para que mantenga "activa" 
 la tarea sass-compile. Cada vez que efectuémos un cambio 
 en el archivo SASS y lo grabemos, la compilación a CSS 
 se re ejecutará automáticamente. */
-gulp.task('default', ()=>{
-	gulp.watch('./scss/*.scss', ['sass-compile']);
+gulp.task('default', function () {
+    gulp.watch('./scss/*.scss');
 });
